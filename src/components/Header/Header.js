1831d6e688dfaf88155c7f74543786ee6105afec
Dani Vijay <danimvijay@gmail.com>
2018-06-08 20:25:53 +0530

    appbar setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import FontAwesome from 'react-font-awesome'
 
 import style from './header.css'
 
 const Header = () => {
+
+  const logo = () => {
+    <Link to="/" className={style.logo}>
+      <img src="/logo.png" alt="wc18 logo"/>
+    </Link>
+  }
+
+  const navBar = () => {
+    return (
+      <FontAwesome name="bars"
+        style={{
+          color: '#dfdfdf',
+          padding: '10px',
+          cursor: pointer
+        }}
+        />
+    )
+  }
+
   return (
     <header>
       <div className={style.header}>
         <div className={style.headeropt}></div>
-        Header
+        {navBar()}
+        {logo()}
       </div>
     </header>
   );
